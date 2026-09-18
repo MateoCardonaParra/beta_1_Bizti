74b4e7a9e6caa24350a56fcfe0da281a04addac1
Leandro <rodriguezleandro998@gmail.com>
2026-09-18 06:24:10 -0500

    feat se crea proyecto

---
+++ beta_1_Bizti/src/main/java/com/example/beta_1_Bizti/Beta1BiztiApplication.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_Bizti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beta1BiztiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Beta1BiztiApplication.class, args);
+	}
+
+}
---
+++ beta_1_Bizti/src/test/java/com/example/beta_1_Bizti/Beta1BiztiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beta_1_Bizti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beta1BiztiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
