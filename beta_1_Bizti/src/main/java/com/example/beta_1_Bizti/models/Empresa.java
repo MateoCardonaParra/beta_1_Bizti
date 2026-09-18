e24c763b0d5cb42759551a61c0a9acc63d713147
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-09-18 07:53:01 -0500

    feat se crea modelo empresa

---
+++ beta_1_Bizti/src/main/java/com/example/beta_1_Bizti/models/Empresa.java
@@ -0,0 +1,122 @@
+package com.example.beta_1_Bizti.models;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empresas")
+public class Empresa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, length = 50)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String nit;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String sector;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String contacto;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String correo;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private String telefono;
+
+    @Column(nullable = false)
+    private Boolean activa = true;
+
+    // Constructor vacío requerido por JPA
+    public Empresa() {
+
+    }
+
+    // Getters y Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Boolean getActiva() {
+        return activa;
+    }
+
+    public void setActiva(Boolean activa) {
+        this.activa = activa;
+    }
+
+    @OneToMany (mappedBy = "empresa")
+    @JsonManagedReference ("reto_empresa")
+    private List <Reto> retos;
+
+}
+
